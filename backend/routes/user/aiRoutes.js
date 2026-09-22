82cf974233a44e51b18d18ed5bcb898677fa3c2a
Savani Avani <savaniavani21@gmail.com>
2026-09-22 19:03:11 +0530

    Add recipe generation route

--- backend/routes/user/aiRoutes.js
+++ backend/routes/user/aiRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const {
   chatWithAI,
+  generateRecipe,
 } = require("../../controllers/user/aiController");
 
 router.post("/chat", chatWithAI);
+router.post("/generate-recipe", generateRecipe);
 
 module.exports = router;
